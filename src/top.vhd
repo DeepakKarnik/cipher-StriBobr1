1f3bdba2292634bc762d8a80866e5fef2a47147c
Deepak Karnik <DeepakKarnik@users.noreply.github.com>
2016-11-16 10:51:12 -0800

    Update top.vhd

--- src/top.vhd
+++ src/top.vhd
@@ -1,60 +1,38 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    23:22:23 11/12/2014 
--- Design Name: 
--- Module Name:    top_1 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- Create Date:    	23:22:23 11/12/2014 
+-- Module Name:    	top_1 - Behavioral 
+-- Project Name:	Design & Evaluation of a cryptographic cipher 
+-- Language:		VHDL
+-- Target Devices: 	Xilinx Virtex 6
+-- Tool versions: 	Xilinx ISE 
+-- Description: 	Top-level datapath module for key generator of StriBob.
+-- Comments:		Stribob is an algorithm for Authenticated Encryption with Associated Data (AEAD).
+--			StriBob is created by Markku-Juhani O. Saarinen as a first round entry
+--			in the CAESAR contest by NIST.
+--			It uses a 512x512 bit permutation PI as its cryptographic foundation.
+--			PI is built from twelve iterations of LPS transformation, interleaved with
+--			EX-OR operation with round constants.
+--			LPS itself stands for Linear, Permutation, Substitution.
+--			All design credits for the algorithm reside with Markku-Juhani O. Saarinen.
+--			This is a VHDL implementation of his algorithm as understood by me.
+--					
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.stribob_pkg.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 -----------------------------------------------------------------------------------
-
-
-
--- NOTE: Cipher deals with 512-bit data. But for checking the behavioral functioning and to
--- quickly enter values, all registers, buses used were 4-bit wide. Since then, port mapping changes
--- are yet to reflect the changed 512-bit buses. Therefore, this file is not yet complete, albeit 
--- successfully passing syntax test. (nov 13)
-
-
-
-
 entity top_1 is
 	
-	port( key: 		in std_logic_vector(511 downto 0);
+	port( 	key: 		in std_logic_vector(511 downto 0);
 			bdi: 		in std_logic_vector(511 downto 0);
 			iv: 		in std_logic_vector(511 downto 0);
-			len_a: 	in std_logic_vector(511 downto 0);
+			len_a: 		in std_logic_vector(511 downto 0);
 			clk: 		in std_logic;
 			bdo:		out std_logic_vector(511 downto 0);
-			
-			
+
 			
 			en_v, en_d, en_c, en_p, en_pi, en_i, en_j: in std_logic;
-			
 			piso_data_en : in std_logic_vector(1 downto 0);
 			
 			sel_v: in std_logic_vector(2 downto 0);
@@ -63,18 +41,18 @@ entity top_1 is
 
 			addr_pad: in std_logic_vector(3 downto 0);
 			
-			 addr_C_constants: in std_logic_vector(3 downto 0);
-			 sel_lps: in std_logic);
+			addr_C_constants: in std_logic_vector(3 downto 0);
+			sel_lps: in std_logic);
 			
 end top_1;
 
 
-
+----------------------------------------------------------------------
 --declaring components of each type of device used
+----------------------------------------------------------------------
 
 architecture Behavioral of top_1 is
 
-			--signal en_v, en_d, en_c, en_p, en_pi, en_i, en_j: std_logic:='1';
 			signal vin, vout, v1, v2, p2, pi_input, pi_out, pi_v : std_logic_vector(511 downto 0);
 			signal din, dout : std_logic_vector(511 downto 0);
 			signal pin, pout : std_logic_vector(511 downto 0);
@@ -84,20 +62,10 @@ architecture Behavioral of top_1 is
 			
 			
 			signal perm: t_stribob_state;
-			
-			
-			--signal sel_v: std_logic_vector(2 downto 0);
-			--signal sel_p: std_logic;
-			--signal sel_d:std_logic_vector(1 downto 0);
-
-			--signal addr_pad: std_logic_vector(3 downto 0);
 			signal pad: std_logic_vector (7 downto 0);
-			
-			--signal addr_C_constants: std_logic_vector(3 downto 0);
 			signal dout_C_constants: std_logic_vector(511 downto 0);
-
 			signal lps: std_logic_vector(511 downto 0);
-			--signal sel_lps: std_logic;
+
 begin
 
 --------------------------------------------------------------------------
@@ -137,7 +105,7 @@ begin
 -- plaintext input
 -----------------------------------------------------------------------------
 	p: entity work.reg_top (Behavioral) generic map(n=>512)
-	port map(enable=>en_p, clk=>clk, d=>vin, q=>pout); --done
+	port map(enable=>en_p, clk=>clk, d=>pin, q=>pout); --done
 
 	pin<= iv when sel_p='0' else
 			p2;
@@ -148,9 +116,6 @@ begin
 	c: entity work.reg_top (Behavioral) generic map(n=>512)
 	port map(enable=>en_c, clk=>clk, d=>cin, q=>cout); --done
 
-
-
-
 ------------------------------------------------------------------------------
 -- pi permutation register
 ------------------------------------------------------------------------------
@@ -159,20 +124,15 @@ lps<= pi_v when sel_lps='0' else
 
 	pi_input<=lps xor dout_C_constants;
 
-	
 	pi: entity work.reg_top (Behavioral) generic map(n=>512) 
 		port map(enable=>en_pi, clk=>clk, d=>pi_input, q=>pi_out);
 
-
-	
-	
 -----------------------------------------------------------------------------
 -- C constants
 --------------------------------------------------------------------------------
 
 	C_constants: entity work.C_round_const (distributed_rom)
 					port map(input=>addr_C_constants, output=>dout_C_constants);
-
 
 
 -----------------------------------------------------------------------
@@ -192,11 +152,10 @@ lps<= pi_v when sel_lps='0' else
 				port map(ip=>pi_out, op=>pi_v);
 
 
-
-
-v1<=	vout xor dout;
-p2<=	cout xor vout;
-cin<= pout xor vout;
-v2<=	vout xor pad;
+v1	<=	vout xor dout;
+p2	<=	cout xor vout;
+cin	<= 	pout xor vout;
+v2	<=	vout xor pad;
 	
 end Behavioral;
+
